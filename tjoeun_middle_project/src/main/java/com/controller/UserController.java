a1b719871ebb8bf001d6dfd851a8e5ae906e08d0
jeong29hyeon <sian0369@naver.com>
2023-05-01 16:51:11 +0900

    pull-test

--- tjoeun_middle_project/src/main/java/com/controller/UserController.java
+++ tjoeun_middle_project/src/main/java/com/controller/UserController.java
@@ -70,7 +70,7 @@ public class UserController {
             ra.addFlashAttribute("msg",user.getName()+"님 환영합니다!");
         } catch (Exception e) {
             ra.addFlashAttribute("msg","ID/PW가 일치하지 않습니다.");
-            return "redirect:/login";
+            return "redirect:/";
         }
         Cookie cookie = new Cookie("id", id);
         if(saveId) {
